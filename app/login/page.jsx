f74fe9642a54dd5c6a5f9adb457ff0e9c75ec554
juan-dio <juanaxl432@gmail.com>
2026-05-12 20:03:00 +0700

    fix: show password bug

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -81,6 +81,7 @@ export default function LoginPage() {
                           type={showPassword ? "text" : "password"}
                         />
                         <button
+                          type="button"
                           onClick={() => setShowPassword(!showPassword)}
                           className="text-slate-500 flex border-none bg-slate-200 items-center justify-center pr-4 rounded-r-xl border-l-0 hover:bg-slate-300 transition-colors"
                         >
@@ -106,7 +107,10 @@ export default function LoginPage() {
                   )}
 
                   <div className="flex px-4 py-3 justify-center w-full">
-                    <button className="flex min-w-[84px] w-full max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 px-5 flex-1 bg-primary text-white text-base font-bold leading-normal tracking-[0.015em] font-display hover:bg-blue-600 transition-colors">
+                    <button
+                      type="submit"
+                      className="flex min-w-[84px] w-full max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 px-5 flex-1 bg-primary text-white text-base font-bold leading-normal tracking-[0.015em] font-display hover:bg-blue-600 transition-colors"
+                    >
                       <span className="truncate">Login</span>
                     </button>
                   </div>
